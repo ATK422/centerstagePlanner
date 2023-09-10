d91c0c93e8cfa213e20ff63bd40b426e24c978e9
ATK <54485972+ATK422@users.noreply.github.com>
2023-09-10 14:55:33 -0400

    Done

---
+++ src/App.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+	import Hexagon from './lib/hexagon.svelte';
+</script>
+
+<main>
+	<div style="style: flex">
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+		<div class="card">
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+			<Hexagon></Hexagon>
+		</div>
+	</div>
+</main>
+
+<style>
+	.card {
+		padding: 0%
+	}
+</style>
---
+++ src/lib/hexagon.svelte
@@ -0,0 +1,50 @@
+<script lang="ts">
+	let state: number = 0
+	let color = "#505050"
+	const update = () => {
+		if (state == 4) {
+			state = 0
+		} else {
+			state += 1
+		}
+
+		if (state == 0) {
+			color = "#505050"
+		} else if (state == 1) {
+			color = "#d8d8d8"
+		} else if (state == 2) {
+			color = "#dda91b"
+		} else if (state == 3) {
+			color = "#22b01d"
+		} else if (state == 4) {
+			color = "#b684c2"
+		}
+	}
+</script>
+
+<style>
+	button {
+		display: inline-block;
+		text-align: center;
+		text-decoration: none;
+		vertical-align: middle;
+		user-select: none;
+		height: 5em;
+		width: 5.2em;
+		rotate: 90deg;
+		
+		outline: none;
+		border: none;
+
+		margin-left: .5em;
+
+		--btn-raise: 1.3rem;
+		clip-path: polygon(var(--btn-raise) 0%, calc(100% - var(--btn-raise)) 0%, 100% 50%, calc(100% - var(--btn-raise)) 100%, var(--btn-raise) 100%, 0 50%);
+		background-color: #dda91b;
+		text-transform: uppercase;
+	}
+</style>
+
+<button on:click={update} style="background-color: {color}">
+	
+</button>
